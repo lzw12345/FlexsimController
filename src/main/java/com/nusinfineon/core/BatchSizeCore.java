0e250ff50f62cb2a463ffab24565f0d7b1de9eb0
AhmadHatziq <e0031550@u.nus.edu>
2020-02-04 14:29:04 +0800

    Commit before updating personal repo

--- src/main/java/com/nusinfineon/core/BatchSizeCore.java
+++ src/main/java/com/nusinfineon/core/BatchSizeCore.java
@@ -1,6 +1,5 @@
 package com.nusinfineon.core;
 
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
