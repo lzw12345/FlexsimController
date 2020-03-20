c7fdf8efe1f8e12179374113074ce0e5a8e12c2d
AhmadHatziq <e0031550@u.nus.edu>
2020-03-20 23:07:57 +0800

    Add handling of missing "Throughput Product Rep" sheet

--- src/main/java/com/nusinfineon/util/OutputAnalysisUtil.java
+++ src/main/java/com/nusinfineon/util/OutputAnalysisUtil.java
@@ -196,6 +196,7 @@ public class OutputAnalysisUtil {
         Cell cycleTimeCell = headerRow.createCell(CYCLE_TIME_COLUMN, CellType.STRING);
         cycleTimeCell.setCellValue("Cycle Time");
 
+        // Exit out if null. Null as sheet doesn't exist in the original excel run. Only exit after writing blank columns.
         if (treeMapOfAverageCycleTimes == null) {
             return;
         }
